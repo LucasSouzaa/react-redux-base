dfd4d227f99fd2dc6b4ecfe97c735a8f42b86e3c
Lucas Silva <lucas.souza@whf.work>
2019-07-01 16:22:42 -0300

    added lazy and suspend to base routes

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import HomeContainer from '../Home/container/HomeContainer';
+const HomeContainer = lazy(() => import('../Home/container/HomeContainer'));
 
 function Index() {
     return <h2>Home</h2>;
@@ -11,21 +11,23 @@ class Navigation extends Component {
     render() {
         return (
             <Router>
-                <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to={'/'}>Home</Link>
-                            </li>
-                            <li>
-                                <Link to={'/page2'}>Pagina 2</Link>
-                            </li>
-                        </ul>
-                    </nav>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to={'/'}>Home</Link>
+                                </li>
+                                <li>
+                                    <Link to={'/page2'}>Pagina 2</Link>
+                                </li>
+                            </ul>
+                        </nav>
 
-                    <Route path="/" exact component={Index} />
-                    <Route path="/page2" exact component={HomeContainer} />
-                </div>
+                        <Route path="/" exact component={Index} />
+                        <Route path="/page2" exact component={HomeContainer} />
+                    </div>
+                </Suspense>
             </Router>
         );
     }
